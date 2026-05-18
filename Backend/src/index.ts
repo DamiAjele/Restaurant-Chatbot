ff04cbebab6daf9f5738bc0ea4375362c32e1e43
Damilola Ajele <damilolaajele05@gmail.com>
2026-05-18 13:37:19 +0100

    FIXED error: route import on entry file

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import apiRouter from "./routes";
+import apiRouter from "./routes/index";
 import { errorHandler } from "./middleware/errorHandler";
 import logger from "./utils/logger";
 
